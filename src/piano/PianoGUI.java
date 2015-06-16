7226e88a5baa85bb73d4eab0fd87f94efe251427
a <a>
2015-06-15 23:01:26 -0500

    t

--- src/piano/PianoGUI.java
+++ src/piano/PianoGUI.java
@@ -86,6 +86,7 @@ public class PianoGUI extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Piano");
         setBackground(new java.awt.Color(51, 51, 51));
+        setResizable(false);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowActivated(java.awt.event.WindowEvent evt) {
                 formWindowActivated(evt);
